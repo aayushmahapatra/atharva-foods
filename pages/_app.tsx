c4a7a4cee4a437f39a257e0f5edf10039009014e
aayushmahapatra <mahapatraaayush@gmail.com>
2023-05-03 12:46:41 +0530

    Fix fonts for deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,9 @@
-import { Poppins } from "next/font/google";
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 
-const poppins = Poppins({
-  weight: ["300", "400", "600", "700"],
-  subsets: ["latin"],
-});
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={poppins.className}>
+    <main>
       <Component {...pageProps} />
     </main>
   );
